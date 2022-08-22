52c9b90932c0700a6d80ad455cd05b62b6cfcc7b
07arif <ai.arif0711@gmail.com>
2022-08-22 20:36:40 +0600

    js added

---
+++ js/case.js
@@ -0,0 +1,40 @@
+/*
+1.add eventlistner to the case plus button
+2.get the value inside the case number field (input field)
+3.convert the new case number 
+4.caculate the new case number
+5.set the value to the case number field
+*/
+
+function updateCaseNumber(isIncrease) {
+
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    let newCaseNumber;
+
+    if (isIncrease === true) {
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+
+    }
+    caseNumberField.value = newCaseNumber;
+    return newCaseNumber;
+}
+
+
+document.getElementById('case-btn-plus').addEventListener('click', function () {
+    const newCaseNumber = updateCaseNumber(true);
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+
+});
+
+document.getElementById('case-number-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
+
+})
